7dc27669243a8419f0e5d3d6b13ab303009b81d9
RadkaGlossova <radka.glossova@seznam.cz>
2023-06-07 17:02:39 +0200

    HW_07(bez reportingu)

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,131 +1,127 @@
+const username = 'da-app.admin@czechitas.cz';
+
+async function openRegistrationPage() {
+    await browser.reloadSession();
+    await browser.url('/registrace');
+}
+
+function getNameField() {
+    return $('#name');
+}
+
+function getEmailField() {
+    return $('#email');
+}
+
+function getPasswordField() {
+    return $('#password');
+}
+
+function getPasswConfirmField() {
+    return $('#password-confirm');
+}
+
+function getSubmitButton() {
+    return $('.btn-primary');
+}
+
+function getToast() {
+    return $('.toast-message');
+}
+
+function getFieldError() {
+    return $('.invalid-feedback');
+}
+
+function getHeader() {
+    return $('h1').getText();
+}
+
+
+
 describe('Homework', async () => {
     beforeEach(async () => {
-        await browser.reloadSession();
-        await browser.url('/registrace');
+       await openRegistrationPage();
     });
     
         
     it('should show login form', async () => {
-        const nameInput = $('#name');
-        //console.log('Name input field is displayed: ' + await nameInput.isDisplayed());
-        //console.log('Name input field is enabled: ' + await nameInput.isEnabled());
+        //kontrola registračního formuláře
+        const nameInput = await getNameField();
         await expect(nameInput).toBeExisting();
         await expect(nameInput).toBeEnabled();
         
-        const emailField = $('#email');
-        // console.log('Email field is displayed: ' + await emailField.isDisplayed());
-        // console.log('Email field is enabled: ' + await emailField.isEnabled());
+        const emailField = await getEmailField();
         await expect(emailField).toBeExisting();
         await expect(emailField).toBeEnabled();
 
-        const passwordInput =  $('#password');
-        //console.log('Password input field is displayed: ' + await passwordInput.isDisplayed());
-        //console.log('Password input field is enabled: ' + await passwordInput.isEnabled());
+        const passwordInput =  await getPasswordField();
         await expect(passwordInput).toBeExisting();
         await expect(passwordInput).toBeEnabled();
                 
-        const passwConfirmInput =  $('#password-confirm');
-        //console.log('Password-confirm input field displayed: ' + await passwConfirmInput.isDisplayed());
-        //console.log('Password-confirm input field is enabled: ' + await passwConfirmInput.isEnabled());
+        const passwConfirmInput =  await getPasswConfirmField();
         await expect(passwConfirmInput).toBeExisting();
         await expect(passwConfirmInput).toBeEnabled();
 
-        const submitButton =  $('.btn-primary');
-        //console.log('Submit button clicable: ' + await submitButton.isClickable());
+        const submitButton =  await getSubmitButton();
         await expect(submitButton).toHaveText('Zaregistrovat','Registrovat');
         await expect(submitButton).toBeClickable();
-
     });
 
 
     it('valid registration', async () => {
-
-                const nameInput = await $('#name');
-                const emailInput = await $('#email');
-                const passwordInput = await $('#password');
-                const passwConfirmInput = await $('#password-confirm');
-                const submitButton = await $('.btn-primary');
-
-                await nameInput.setValue('jajinek');
-                await emailInput.setValue('jajinek14@seznam.cz');
-                await passwordInput.setValue('Jajinek,123');
-                await passwConfirmInput.setValue('Jajinek,123');
-                await submitButton.click();
+                // vyplní formulář
+                await getNameField().setValue('jajinek');
+                await getEmailField().setValue('jajinek18@seznam.cz');
+                await getPasswordField().setValue('Jajinek,123');
+                await getPasswConfirmField().setValue('Jajinek,123');
+                await getSubmitButton().click();
                 await browser.pause(2000);
-
-                const nadpis = await $('h1').getText();
-                await expect(await nadpis).toEqual('Přihlášky');
-                //console.log(nadpis);
-        
+                
+                // kontrola přesměrování z registrační stránky
+                await expect(await getHeader()).toEqual('Přihlášky');
     });
 
     it('registration with existing email', async () =>{
-        
-        const nameInput = await $('#name');
-        const emailInput = await $('#email');
-        const passwordInput = await $('#password');
-        const passwConfirmInput = await $('#password-confirm');
-        const submitButton = await $('.btn-primary');
+        //vyplní existující email
+        await getNameField().setValue('jajinek');
+        await getEmailField().setValue(username);
+        await getPasswordField().setValue('Jajinek,123');
+        await getPasswConfirmField().setValue('Jajinek,123');
+        await getSubmitButton().click();
 
-        await nameInput.setValue('jajinek');
-        await emailInput.setValue('jajinek3@seznam.cz');
-        await passwordInput.setValue('Jajinek,123');
-        await passwConfirmInput.setValue('Jajinek,123');
-        await submitButton.click();
+        // kontrola - neproběhne registace/přesměrování
+        await expect(await getHeader()).toEqual('Registrace');
 
-        const toastTitle = await $('.toast-title');
-        const toastMessage = await $('.toast-message');
-        const emailError = await $('.invalid-feedback');
-
-        const nadpis = await $('h1').getText();
-
-        await expect(nadpis).toEqual('Registrace');
-        await expect(toastTitle).toBeDisplayed();
-        await expect(toastTitle).toHaveTextContaining('Špatně','špatně');
-        await expect(toastMessage).toBeDisplayed();
-        await expect(toastMessage).toHaveTextContaining('špatně')
-        await expect(emailError).toBeDisplayed();
-        await expect(emailError).toHaveTextContaining('email');
-        //console.log(nadpis)
-        //console.log(toastTitle);
-        //console.log(toastMessage);
-        //console.log(emailError);
-
+        // kontrola chybových hlášek
+        // await expect(toastTitle).toBeDisplayed();
+        // await expect(toastTitle).toHaveTextContaining('Špatně','špatně');
+        await expect(getToast()).toBeDisplayed();
+        await expect(getToast()).toHaveTextContaining('špatně')
+        await expect(getFieldError()).toBeDisplayed();
+        await expect(getFieldError()).toHaveTextContaining(['email','emailem']);
 
     }),
     it('registration with invalid password', async () =>{
         
-        const nameInput = await $('#name');
-        const emailInput = await $('#email');
-        const passwordInput = await $('#password');
-        const passwConfirmInput = await $('#password-confirm');
-        const submitButton = await $('.btn-primary');
-
-        await nameInput.setValue('jajinek');
-        await emailInput.setValue('jajinek5@seznam.cz');
-        await passwordInput.setValue('123');
-        await passwConfirmInput.setValue('123');
-        await submitButton.click();
-
-        const toastTitle = await $('.toast-title');
-        const toastMessage = await $('.toast-message');
-        const passwordError = await $('.invalid-feedback');
-
-        const nadpis = await $('h1').getText();
+        // vyplní špatné heslo
+        await getNameField().setValue('jajinek');
+        await getEmailField().setValue('jajinek22@seznam.cz');
+        await getPasswordField().setValue('123');
+        await getPasswConfirmField().setValue('123');
+        await getSubmitButton().click();
 
         
-        await expect(nadpis).toEqual('Registrace');
-        await expect(await toastTitle).toBeDisplayed();
-        await expect(await toastMessage).toHaveTextContaining(['špatně','Špatně'])
-        await expect(await toastMessage).toBeDisplayed();
-        await expect(await toastMessage).toHaveTextContaining(['špatně','Špatně'])
-        await expect(await passwordError).toBeDisplayed();
-        await expect(await passwordError).toHaveTextContaining(['heslo','Heslo']);
-
-        //console.log(nadpis)
-        //console.log(toastTitle);
-        //console.log(toastMessage);
-        //console.log(PasswordError);
+        // kontrola, že neproběhne registrace/přesměrování        
+        await expect(await getHeader()).toEqual('Registrace');
+        
+        // kontrola chybových hlášek
+        //await expect(await toastTitle).toBeDisplayed();
+        //await expect(await toastTitle).toHaveTextContaining(['špatně','Špatně'])
+        await expect(await getToast()).toBeDisplayed();
+        await expect(await getToast()).toHaveTextContaining(['špatně','Špatně'])
+        await expect(await getFieldError()).toBeDisplayed();
+        await expect(await getFieldError()).toHaveTextContaining(['heslo','Heslo']);
 
 
     });
