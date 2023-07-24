b112d4b80ce10a654afa2d31b9f2204334fb9a92
RadkaGlossova <radka.glossova@seznam.cz>
2023-07-24 08:25:00 +0200

    homework finished

--- test/specs/fixtures.js
+++ test/specs/fixtures.js
@@ -1,5 +1,5 @@
-export const username = '';
-export const password = '';
+export const username = 'da-app.admin@czechitas.cz';
+export const password = 'Czechitas123';
 export const userFullName = 'Lišák Admin';
 export const expectedApplicationsPageRows = 30;
 export const searchText = 'mari';
